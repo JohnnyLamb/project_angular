fa927c7f63d372f161a8863b9c6636d701680a82
johnny lamb <agnusjack@gmail.com>
2015-10-05 12:05:21 -0600

    trying to setup authentication

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/users');
+var passport = require('passport');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req.user);
+  res.render('signup');
 });
+
+router.get('/register', function(req, res) {
+  res.render('register');
+});
+
+// router.post('/', function(req, res, next) {
+//   var newUser = new User({
+//     username: req.body.username,
+//     password: req.body.password
+//   });
+//   newUser.save(function(err, data) {
+//     if (err) {
+//       res.json({
+//         'message': err
+//       });
+//     } else {
+//       res.json(data);
+//     }
+//   });
+// });
 
 module.exports = router;
